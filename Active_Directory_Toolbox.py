1eea468239c4a47ab0ada3ad70af4d0c0572d489
MattRahlfs <43938137+MattRahlfs@users.noreply.github.com>
2023-04-09 15:30:40 -0600

    Accounts, Computers, Groups
    Able to access multiple "pages" to perform certain actions

--- Active_Directory_Toolbox.py
+++ Active_Directory_Toolbox.py
@@ -1,44 +1,81 @@
 import tkinter
-
-def nothing():
-    print("testing")
+import time
     
-
 
 class ActiveDirectoryToolBox():
     
-    def adAccountMenuCanvas():
-        pass
+    
+    def __init__(self) -> None:
+
+        def clearFrame(frame):
+            for widget in frame.winfo_children():
+                widget.destroy()
+
+        def adAccountMenuWidgets():
+            clearFrame(self.mainFrame)
+            titleLabel = tkinter.Label(self.mainFrame, text="Account Management", font=("Arial", 15))
+            titleLabel.pack(pady=5)
+
+            adAccountIdentityTextboxLabel = tkinter.Label(self.mainFrame, text="Account Identity", font=("Arial", 8))
+            adAccountIdentityTextboxLabel.pack(side=tkinter.LEFT, padx=5)
+
+            adAccountIdentityTextboxWidget = tkinter.Entry(self.mainFrame)
+            adAccountIdentityTextboxWidget.pack()
+
+            getAccount=tkinter.Button(self.mainFrame, text="acc info")
+            getAccount.pack()
+
+        
+        def adComputerMenuWidgets():
+            clearFrame(self.mainFrame)
+
+            titleLabel = tkinter.Label(self.mainFrame, text="Computer Management", font=("Arial", 15))
+            titleLabel.pack()
+
+        def adGroupMenuWidgets():
+            clearFrame(self.mainFrame)
+
+            titleLabel = tkinter.Label(self.mainFrame, text="Group Management", font=("Arial", 15))
+            titleLabel.pack()
+
+        def nothing():
+            print("testing")
+
+
+        self.rootCanvas = tkinter.Tk()
+
+        self.rootCanvas.title("AD Toolbox")
+        self.rootCanvas.geometry('400x400')
+        self.rootCanvas.resizable(False,False)
+        self.rootCanvas.eval('tk::PlaceWindow . center')
+
+
+        self.mainFrame = tkinter.Frame(self.rootCanvas)
+        self.mainFrame.pack()
+        self.mainMenuNavBar = tkinter.Menu(self.rootCanvas)
+
+        
+        fileMenu = tkinter.Menu(self.mainMenuNavBar, tearoff=0)
+        fileMenu.add_command(label="Save State", command=nothing)
+        fileMenu.add_command(label="Notes", command=nothing)
+        fileMenu.add_command(label="Exit", command=self.rootCanvas.quit)
+        self.mainMenuNavBar.add_cascade(label="File", menu=fileMenu)
+
+        
+        
+        self.mainMenuNavBar.add_command(label="Accounts", command=adAccountMenuWidgets)
+        self.mainMenuNavBar.add_command(label="Computers", command=adComputerMenuWidgets)
+        self.mainMenuNavBar.add_command(label="Groups", command=adGroupMenuWidgets)
+        self.rootCanvas.config(menu=self.mainMenuNavBar)
+
+
+
+        adAccountMenuWidgets()                  
+        self.rootCanvas.mainloop()
     
     
-    rootCanvas = tkinter.Tk()
 
-    rootCanvas.title("AD Toolbox")
-    rootCanvas.geometry('400x400')
-    rootCanvas.resizable(False,False)
-    rootCanvas.eval('tk::PlaceWindow . center')
-
-
-    mainMenuNavBar = tkinter.Menu(rootCanvas)
     
-    fileMenu = tkinter.Menu(mainMenuNavBar, tearoff=0)
-    fileMenu.add_command(label="Save State", command=nothing)
-    fileMenu.add_command(label="Notes", command=nothing)
-    fileMenu.add_command(label="Exit", command=rootCanvas.quit)
-    mainMenuNavBar.add_cascade(label="File", menu=fileMenu)
-    
-    
-    mainMenuNavBar.add_command(label="Accounts", command=adAccountMenuCanvas)
-    mainMenuNavBar.add_command(label="Computers", command=nothing)
-    mainMenuNavBar.add_command(label="Groups", command=nothing)
-    
-    label = tkinter.Label(rootCanvas, text="hello world")
-    label.pack()
-
-
-    rootCanvas.config(menu=mainMenuNavBar)
-    rootCanvas.mainloop()
-
 
 
 ###########################################################################################################################
